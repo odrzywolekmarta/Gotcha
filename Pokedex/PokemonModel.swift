28310081cd9ef0a887f17dd8bfd851b09df6024e
Marta Odrzywolek <odrzywolek.m@gmail.com>
2022-10-26 14:54:04 +0200

    add PokemonModel

---
+++ Pokedex/PokemonModel.swift
@@ -0,0 +1,41 @@
+//
+//  PokemonModel.swift
+//  Pokedex
+//
+//  Created by Majka on 26/10/2022.
+//
+
+import Foundation
+
+struct Pokemon: Decodable {
+    let id: Int
+    let name: String
+    let abilities: [Ability]
+    let sprites: Sprite
+    let types: [PokemonType]
+}
+
+struct Ability: Decodable {
+    let ability: AbilityName
+}
+
+struct AbilityName: Decodable {
+    let name: String
+}
+
+struct Sprite: Decodable {
+    let frontDefault: URL
+    
+    enum CodingKeys: String, CodingKey {
+        case frontDefault = "front_default"
+    }
+    
+}
+
+struct PokemonType: Decodable {
+    let type: TypeName
+}
+
+struct TypeName: Decodable {
+    let name: String
+}
