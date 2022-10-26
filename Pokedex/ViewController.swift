092b2ebadfe2100da90ab723a38408c564092c41
Marta Odrzywolek <odrzywolek.m@gmail.com>
2022-10-26 13:08:24 +0200

    initial commit

---
+++ Pokedex/ViewController.swift
@@ -0,0 +1,19 @@
+//
+//  ViewController.swift
+//  Pokedex
+//
+//  Created by Majka on 26/10/2022.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view.
+    }
+
+
+}
+
