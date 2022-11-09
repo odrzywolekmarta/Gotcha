8a1ffb214f1f9218b188d50812200c0692a26727
Marta Odrzywolek <odrzywolek.m@gmail.com>
2022-11-09 15:57:18 +0100

    add app icon

--- Pokedex/PokemonTableViewCell.swift
+++ Pokedex/PokemonTableViewCell.swift
@@ -16,6 +16,7 @@ class PokemonTableViewCell: UITableViewCell {
     
     override func awakeFromNib() {
         super.awakeFromNib()
+        
     }
 
     override func setSelected(_ selected: Bool, animated: Bool) {
