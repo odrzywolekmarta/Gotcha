9d53a8c0c4d550d9d00f3de503939f575a4ee255
Marta Odrzywolek <odrzywolek.m@gmail.com>
2022-11-15 14:20:59 +0100

    do ui changes

--- Pokedex/AboutViewController.swift
+++ Pokedex/AboutViewController.swift
@@ -13,6 +13,7 @@ class AboutViewController: UIViewController {
     @IBOutlet weak var heightLabel: UILabel!
     @IBOutlet weak var abilityLabel1: UILabel!
     @IBOutlet weak var abilityLabel2: UILabel!
+    @IBOutlet weak var abilityLabel3: UILabel!
     @IBOutlet weak var typeLabel1: UILabel!
     @IBOutlet weak var typeLabel2: UILabel!
     
@@ -22,9 +23,56 @@ class AboutViewController: UIViewController {
         super.viewDidLoad()
         viewModel.delegate = self
         view.backgroundColor = UIColor(named: Constants.Colors.customBeige)
-        
+        typeLabel1.makeRound(radius: 12)
+        typeLabel2.makeRound(radius: 14)
     }
 }
+
+func getColor(for type: String) -> UIColor {
+    let pokemonType = type
+    switch pokemonType {
+    case "normal":
+        return UIColor(named: Constants.Colors.normalType)!
+    case "fire":
+        return UIColor(named: Constants.Colors.fireType)!
+    case "water":
+        return UIColor(named: Constants.Colors.waterType)!
+    case "grass":
+        return UIColor(named: Constants.Colors.grassType)!
+    case "electric":
+        return UIColor(named: Constants.Colors.electricType)!
+    case "ice":
+        return UIColor(named: Constants.Colors.iceType)!
+    case "fighting":
+        return UIColor(named: Constants.Colors.fightingType)!
+    case "poison":
+        return UIColor(named: Constants.Colors.poisonType)!
+    case "ground":
+        return UIColor(named: Constants.Colors.groundType)!
+    case "flying":
+        return UIColor(named: Constants.Colors.flyingType)!
+    case "psychic":
+        return UIColor(named: Constants.Colors.psychicType)!
+    case "bug":
+        return UIColor(named: Constants.Colors.bugType)!
+    case "rock":
+        return UIColor(named: Constants.Colors.rockType)!
+    case "ghost":
+        return UIColor(named: Constants.Colors.ghostType)!
+    case "dark":
+        return UIColor(named: Constants.Colors.darkType)!
+    case "dragon":
+        return UIColor(named: Constants.Colors.dragonType)!
+    case "steel":
+        return UIColor(named: Constants.Colors.steelType)!
+    case "fairy":
+        return UIColor(named: Constants.Colors.fairyType)!
+    default:
+        return UIColor(named: Constants.Colors.normalType)!
+    }
+}
+
+//MARK: - Table View Delegate Methods
 
 extension AboutViewController: AboutViewModelDelegate {
     func onDetailsModelSet() {
@@ -51,13 +99,44 @@ extension AboutViewController: AboutViewModelDelegate {
                 switch numOfAbilities {
                 case 1:
                     self.abilityLabel1.text = model.abilities[0].ability.name
+                    self.abilityLabel2.isHidden = true
+                    self.abilityLabel3.isHidden = true
                 case 2:
                     self.abilityLabel1.text = model.abilities[0].ability.name
                     self.abilityLabel2.text = model.abilities[1].ability.name
-                default:
+                    self.abilityLabel3.isHidden = true
+                case 3:
                     self.abilityLabel1.text = model.abilities[0].ability.name
+                    self.abilityLabel2.text = model.abilities[1].ability.name
+                    self.abilityLabel3.text = model.abilities[2].ability.name
+                default:
+                    self.abilityLabel1.isHidden = true
+                    self.abilityLabel2.isHidden = true
+                    self.abilityLabel3.isHidden = true
                 }
                 
+                let numOfTypes = model.types.count
+                switch numOfTypes {
+                case 1:
+                    self.typeLabel1.text = model.types[0].type.name
+                    let typeColor1 = getColor(for: model.types[0].type.name)
+                    self.typeLabel1.backgroundColor = typeColor1
+//                    self.typeLabel2.text = "label"
+                    self.typeLabel2.textColor = .clear
+                    self.typeLabel2.backgroundColor = .clear
+                case 2:
+                    self.typeLabel1.text = model.types[0].type.name
+                    self.typeLabel2.text = model.types[1].type.name
+                    let typeColor1 = getColor(for: model.types[0].type.name)
+                    let typeColor2 = getColor(for: model.types[1].type.name)
+                    self.typeLabel1.backgroundColor = typeColor1
+                    self.typeLabel2.backgroundColor = typeColor2
+                default:
+                    self.typeLabel1.isHidden = true
+                    self.typeLabel2.isHidden = true
+                }
+                
+               
             }
         }
     }
