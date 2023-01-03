5164fd56f85d915291b48ece56a434eec6c38824
Marta Odrzywolek <odrzywolek.m@gmail.com>
2023-01-03 16:34:25 +0100

    change ability button color

--- Pokedex/AboutViewController.swift
+++ Pokedex/AboutViewController.swift
@@ -32,7 +32,6 @@ class AboutViewController: UIViewController {
         typeLabel2.makeRound(radius: typeLabel2.frame.height / 2)
         typeLabel1.isHidden = true
         typeLabel2.isHidden = true
-        
         abilityButton1.titleLabel?.font = UIFont(name: Constants.customFontBold, size: 17)
         abilityButton2.titleLabel?.font = UIFont(name: Constants.customFontBold, size: 17)
         abilityButton3.titleLabel?.font = UIFont(name: Constants.customFontBold, size: 17)
