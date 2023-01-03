ca6f2683c34f7e039176922dd5d7185ac217d82b
Marta Odrzywolek <odrzywolek.m@gmail.com>
2023-01-03 15:23:25 +0100

    handle eevee case

--- Pokedex/EvolutionViewController.swift
+++ Pokedex/EvolutionViewController.swift
@@ -24,12 +24,18 @@ class EvolutionViewController: UIViewController {
     @IBOutlet weak var secondButton: UIButton!
     @IBOutlet weak var thirdButton: UIButton!
     @IBOutlet weak var fourthButton: UIButton!
+    @IBOutlet weak var eeveeView: UIView!
+    @IBOutlet var eeveeViewsCollection: [UIView]!
+    @IBOutlet var eeveeButtonsCollection: [UIButton]!
+    
+    
+    
     
     let viewModel: EvolutionViewModelProtocol = EvolutionViewModel()
     let router: AppRouterProtocol
-    var baseEvolutionId: String = ""
-    var firstEvolutionId: String = ""
-    var secondEvolutionId: String = ""
+    private var baseEvolutionId: String = ""
+    private var firstEvolutionId: String = ""
+    private var secondEvolutionId: String = ""
     
     override func viewDidLoad() {
         super.viewDidLoad()
@@ -49,6 +55,14 @@ class EvolutionViewController: UIViewController {
         thirdView.applyShadow()
         fourthView.makeRound()
         fourthView.applyShadow()
+        eeveeView.isHidden = true
+        for view in eeveeViewsCollection {
+            view.makeRound()
+            view.applyShadow()
+        }
+        for btn in eeveeButtonsCollection {
+            btn.titleLabel?.isHidden = true
+        }
     }
     
     init(router: AppRouterProtocol) {
@@ -63,12 +77,19 @@ class EvolutionViewController: UIViewController {
  
     @IBAction func goToDetails(_ sender: UIButton) {
         let baseUrl = "https://pokeapi.co/api/v2/pokemon/"
-        if sender == firstButton {
-            router.navigateToDetails(urlString: "\(baseUrl)\(baseEvolutionId)")
-        } else if sender == secondButton || sender == thirdButton {
-            router.navigateToDetails(urlString: "\(baseUrl)\(firstEvolutionId)")
-        } else if sender == fourthButton {
-            router.navigateToDetails(urlString: "\(baseUrl)\(secondEvolutionId)")
+        if eeveeView.isHidden {
+            if sender == firstButton {
+                router.navigateToDetails(urlString: "\(baseUrl)\(baseEvolutionId)")
+            } else if sender == secondButton || sender == thirdButton {
+                router.navigateToDetails(urlString: "\(baseUrl)\(firstEvolutionId)")
+            } else if sender == fourthButton {
+                router.navigateToDetails(urlString: "\(baseUrl)\(secondEvolutionId)")
+            }
+        } else  {
+            if let id = sender.titleLabel?.text {
+                router.navigateToDetails(urlString: "\(baseUrl)\(id)")
+                
+            }
         }
     }
     
@@ -80,20 +101,25 @@ extension EvolutionViewController: EvolutionViewModelDelegate {
     func onEvolutionModelFetchSuccess() {
         DispatchQueue.main.async {
             if let evolutionArray = self.viewModel.simplifiedEvolutionSetModel?.evolutionsIdsArray {
+                let eeveeIds = ["133", "134", "135", "136", "196", "197", "470", "471", "700"]
                 let chainCount = self.viewModel.simplifiedEvolutionSetModel?.evolutionsIdsArray.count
                 switch chainCount {
                 case 1:
                     self.noEvolutionLabel.isHidden = false
                 case 2:
-                    self.firstEvolutionStackView.isHidden = false
-                    let firstUrl = self.viewModel.getPokemonImageUrl(forSpeciesId: self.viewModel.simplifiedEvolutionSetModel?.evolutionsIdsArray[0])
-                    let secondUrl = self.viewModel.getPokemonImageUrl(forSpeciesId: self.viewModel.simplifiedEvolutionSetModel?.evolutionsIdsArray[1])
-                    self.basePokemonImage.sd_setImage(with: firstUrl)
-                    self.firstEvolutionImage.sd_setImage(with: secondUrl)
-                    self.secondBasePokemonImage.sd_setImage(with: secondUrl)
-                    self.secondEvolutionStackView.isHidden = true
                     self.baseEvolutionId = evolutionArray[0]
                     self.firstEvolutionId = evolutionArray[1]
+                    if eeveeIds.contains(self.baseEvolutionId) {
+                        self.eeveeView.isHidden = false
+                    } else {
+                        self.firstEvolutionStackView.isHidden = false
+                        let firstUrl = self.viewModel.getPokemonImageUrl(forSpeciesId: self.viewModel.simplifiedEvolutionSetModel?.evolutionsIdsArray[0])
+                        let secondUrl = self.viewModel.getPokemonImageUrl(forSpeciesId: self.viewModel.simplifiedEvolutionSetModel?.evolutionsIdsArray[1])
+                        self.basePokemonImage.sd_setImage(with: firstUrl)
+                        self.firstEvolutionImage.sd_setImage(with: secondUrl)
+                        self.secondBasePokemonImage.sd_setImage(with: secondUrl)
+                        self.secondEvolutionStackView.isHidden = true
+                    }
                 case 3:
                     self.firstEvolutionStackView.isHidden = false
                     self.secondEvolutionStackView.isHidden = false
