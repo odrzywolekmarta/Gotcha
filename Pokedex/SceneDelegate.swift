03078d77201b622b7c5c268e419e6de5d5bf18f2
Marta Odrzywolek <odrzywolek.m@gmail.com>
2023-03-28 22:16:33 +0200

    add app url

--- Pokedex/SceneDelegate.swift
+++ Pokedex/SceneDelegate.swift
@@ -11,6 +11,12 @@ class SceneDelegate: UIResponder, UIWindowSceneDelegate {
     
     var window: UIWindow?
     
+    func scene(_ scene: UIScene, openURLContexts URLContexts: Set<UIOpenURLContext>) {
+        print(URLContexts.first?.url)
+        
+        print(URLContexts.first?.url.absoluteString)
+    }
+    
     func scene(_ scene: UIScene, willConnectTo session: UISceneSession, options connectionOptions: UIScene.ConnectionOptions) {
         guard let _ = (scene as? UIWindowScene) else { return }
         
