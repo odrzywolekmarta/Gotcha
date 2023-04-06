3523d67b03511a03118250bb66cdbd1270bbf8fe
Marta Odrzywolek <odrzywolek.m@gmail.com>
2023-04-06 19:08:10 +0200

    add types images

---
+++ Pokedex/Types/TypeCollectionViewCell.swift
@@ -0,0 +1,17 @@
+//
+//  TypeCollectionViewCell.swift
+//  Gotcha
+//
+//  Created by Majka on 31/03/2023.
+//
+
+import UIKit
+
+class TypeCollectionViewCell: UICollectionViewCell {
+
+    override func awakeFromNib() {
+        super.awakeFromNib()
+        // Initialization code
+    }
+
+}
